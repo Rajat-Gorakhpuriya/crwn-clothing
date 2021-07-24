c1b309079a07ea939a9db178407d2f906fe12284
Rajat Gorakhpuriya <rajat.gorakhpuriya@innoeye.com>
2021-07-24 13:51:25 +0530

    feat: add  cart component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reduser";
+import cartReducer from "./cart/cart.reducer";
 
 export default combineReducers({
-    users: userReducer
+    users: userReducer,
+    cart: cartReducer
 }) 
